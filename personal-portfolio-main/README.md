a930d3488eb7ed52e195b27dcea802de91e22ef6
2200031294 <147637978+2200031294@users.noreply.github.com>
2024-06-20 10:48:55 +0530

    Add files via upload

---
+++ personal-portfolio-main/README.md
@@ -0,0 +1,43 @@
+# Personal Portfolio
+This is my first attempt to create a personal portfolio website to place some of my projects, using basic front-end technologies.
+
+Check [here](https://pennylioliou.netlify.app/)
+
+## Overview
+
+![2022-04-18 17-02-14 (online-video-cutter com) (1)](https://user-images.githubusercontent.com/88145246/163820952-46a0b86b-74ac-4a5e-9510-4e502a9e013d.gif)
+
+## Features
+* Responsive
+* Modern Design
+* Clean Animations
+* Easy to use
+
+## Languages and Tools
+* HTML5
+* CSS3
+* JS
+
+## For the Future
+&#8594; Add Vertical Timeline to put Summary, Skills, Education and Experience
+
+## The MIT License
+Copyright © 2021 Penny Lioliou
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## References
+* [project-3.jpg](https://www.istockphoto.com/photo/cloud-computing-emerging-from-computer-servers-technology-innovation-gm539475910-96201179)
+* [project-4.jpg](https://www.istockphoto.com/photo/macro-shot-with-augmented-reality-ethernet-cables-connected-to-router-ports-with-gm1219430161-356699621)
+* [project-5.jpg](https://13.127.97.79/course/compiler-design-online-videos)
+
+
+ If you like this portfolio template, make sure to ⭐ the repository to make it more recognizable for other users. Thank you!
+
+![myportfolio](https://user-images.githubusercontent.com/88145246/218483901-cbf811a0-9c92-4443-9c5f-31712a07ed80.jpg)
+
+
---
+++ personal-portfolio-main/script.js
@@ -0,0 +1,105 @@
+/* Progress bar Effect */
+const navbar = document.querySelector(".navbar");
+const navbarOffsetTop = navbar.offsetTop;
+const sections = document.querySelectorAll("section");
+const navbarLinks = document.querySelectorAll(".navbar-link");
+const progress = document.querySelector(".progress-bars-wrapper");
+const progressBarPercents = [97, 90, 72, 80, 48];
+
+window.addEventListener("scroll", () => {
+  mainFn();
+});
+
+const mainFn = () => {
+  if (window.pageYOffset >= navbarOffsetTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+
+  sections.forEach((section, i) => {
+    if (window.pageYOffset >= section.offsetTop - 10) {
+      navbarLinks.forEach((navbarLink) => {
+        navbarLink.classList.remove("change");
+      });
+      navbarLinks[i].classList.add("change");
+    }
+  });
+
+  if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+    document.querySelectorAll(".progress-percent").forEach((el, i) => {
+      el.style.width = `${progressBarPercents[i]}%`;
+      el.previousElementSibling.firstElementChild.textContent =
+        progressBarPercents[i];
+    });
+  }
+};
+
+mainFn();
+
+window.addEventListener("resize", () => {
+  window.location.reload();
+});
+
+
+/* Typing and Deleting Effect */
+// List of sentences
+var _CONTENT = [ 
+	"SOFTWARE ENGINEER", 
+	"FRONT-END DEVELOPER", 
+	"BUSINESS APP DEVELOPER"
+];
+
+var _PART = 0; // Current sentence being processed
+var _PART_INDEX = 0; // Character number of the current sentence being processed 
+var _INTERVAL_VAL; // Holds the handle returned from setInterval
+var _ELEMENT = document.querySelector("#text-name"); // Element that holds the text
+var _CURSOR = document.querySelector("#cursor"); // Cursor element 
+
+// Implements typing effect
+function Type() { 
+	// Get substring with 1 characater added
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX++;
+
+	// If full sentence has been displayed then start to delete the sentence after some time
+	if(text === _CONTENT[_PART]) {
+		// Hide the cursor
+		_CURSOR.style.display = 'none';
+
+		clearInterval(_INTERVAL_VAL);
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Delete, 50);
+		}, 1000);
+	}
+}
+
+// Implements deleting effect
+function Delete() {
+	// Get substring with 1 characater deleted
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX--;
+
+	// If sentence has been deleted then start to display the next sentence
+	if(text === '') {
+		clearInterval(_INTERVAL_VAL);
+
+		// If current sentence was last then display the first one, else move to the next
+		if(_PART == (_CONTENT.length - 1))
+			_PART = 0;
+		else
+			_PART++;
+		_PART_INDEX = 0;
+
+		// Start to display the next sentence after some time
+		setTimeout(function() {
+			_CURSOR.style.display = 'inline-block';
+			_INTERVAL_VAL = setInterval(Type, 100);
+		}, 200);
+	}
+}
+// Start the typing effect on load
+_INTERVAL_VAL = setInterval(Type, 100);
+
